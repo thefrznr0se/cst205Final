078aafe23680004c62673d1ee2a62adea342404f
Aaron Martinez <aaronmartinez@Comp.attlocal.net>
2023-12-14 10:56:16 -0800

    added login and ability to create an account. Also added the map to display route.

--- Final_Project.py
+++ Final_Project.py
@@ -1,21 +1,24 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap5
+from user_info import user_info
 import requests
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 
 @app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=('GET', 'POST'))
 def test():
-    if request.method == 'POST':
-        origin = request.form['origin']
-        destination = request.form['destination']
+    if request.method == "POST":
+        user = request.form['username']
+        password = request.form['Password']
 
-        route_info = get_route_info(origin, destination)
-    
-        return render_template('index.html', route_info=route_info, origin=origin, destination=destination)
-    
-    return render_template('index.html')
+        size = len(user_info)
+        for x in range(size):
+            if user_info[x]['username'] == user and user_info[x]['password'] == password:
+                return render_template('index.html')
+
+    return render_template('login.html')
 
 @app.route('/index', methods=['GET', 'POST'])
 def index():
@@ -24,8 +27,9 @@ def index():
         destination = request.form['destination']
 
         route_info = get_route_info(origin, destination)
+        static_map_url = get_route_info2(origin, destination)
 
-        return render_template('index.html', route_info=route_info, origin=origin, destination=destination)
+        return render_template('index.html', route_info=route_info, static_map_url=static_map_url, origin=origin, destination=destination)
     
     return render_template('index.html')
 
@@ -44,5 +48,20 @@ def get_route_info(origin, destination):
     data = response.json()
     return data['route']
 
+def get_route_info2(origin, destination):
+    return 'https://mapquestapi.com/staticmap/v5/map?start=' + f'{origin}' + '|flag-start&end=' + f'{destination}' +  '|flag-end&type=dark&size=1000,400@2x&traffic=flow|cons|&key=0ZOdZgHrvADP8FzwKEvjXVf8VCGLmUjy'
 
-app.run()
+@app.route('/create_account', methods=('GET', 'POST'))
+def create_account():
+    if request.method == "POST":
+        user = request.form['username']
+        password = request.form['Password']
+
+        user_info.append({
+        'username': user,
+        'password': password
+        })
+        
+        return render_template('login.html')
+
+    return render_template('create_account.html')
---
+++ user_info.py
@@ -0,0 +1,14 @@
+# user_info.py
+
+user_info = [
+    {
+        'username': 'user',
+        'password': 'user',
+        'name': 'User One'
+    },
+    {
+        'username': 'user2',
+        'password': 'password2',
+        'name': 'User Two'
+    },
+]
